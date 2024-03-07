7e8f16314bbdf6499aeda9fa1b555de94a09cf16
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-03-07 16:13:58 +0100

    feat: add link to online mongodb database

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 ## Description
 
-
+This is the API of Heya the Lab3 prject of Tjerk, Wolf and Wouter. Heya is a platform for people to rent out a room in their house to students. The API is built with NestJS and uses a MongoDB database.
 
 ## Installation
 
 ```bash
 $ npm install
 ```
+
+create a .env file in the root of the project and add the following variables:
+
+`MONGODB=Heya_mongodb_connection_string`
 
 ## Running the app
 
--- src/app.module.js
+++ src/app.module.js
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './users/user.module';
 import { RoomModule } from './rooms/room.module';
 import { OwnerModule } from './owners/owner.module';
@@ -6,7 +8,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [UserModule, RoomModule, OwnerModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB', 'mongodb://localhost:27017'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule, 
+    RoomModule, 
+    OwnerModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
