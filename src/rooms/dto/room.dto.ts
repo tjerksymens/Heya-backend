ecf889b4ea4e2be8a4da3dcad2bd9560b26e1ae1
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-03-21 13:35:17 +0100

    feat: expanding the schema and dto of room with al possible info

--- src/rooms/dto/room.dto.ts
+++ src/rooms/dto/room.dto.ts
@@ -1,12 +1,90 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsArray } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsArray, IsBoolean, IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class RoomDto {
     @ApiProperty()
     @IsString()
     public title: string;
 
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    public description: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    public extraInfo: string;
+
+    @ApiProperty()
+    @IsBoolean()
+    public furnished: boolean;
+
+    @ApiPropertyOptional()
+    @IsNumber()
+    @IsOptional()
+    public houseSize: number;
+
+    @ApiProperty()
+    @IsNumber()
+    public amountOfRooms: number;
+
+    @ApiProperty()
+    @IsNumber()
+    public PeopleLivingHere: number;
+
+    @ApiProperty()
+    @IsBoolean()
+    public areTherePets: boolean;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    public kindOfPets: string;
+
+    @ApiProperty()
+    @IsString()
+    public imageLink: string;
+
+    @ApiProperty()
+    @IsDateString()
+    public startDateAvailability: string;
+
+    @ApiProperty()
+    @IsDateString()
+    public endDateAvailability: string;
+
+    @ApiProperty()
+    @IsNumber()
+    public rentPrice: number;
+
+    @ApiProperty()
+    @IsNumber()
+    public additionalCost: number;
+
+    @ApiProperty()
+    @IsNumber()
+    public serviceCost: number;
+
+    @ApiProperty()
+    @IsNumber()
+    public deposit: number;
+
+    @ApiPropertyOptional()
+    @IsArray()
+    @IsOptional()
+    public idealAttendantInfo: string[];
+
+    @ApiPropertyOptional()
+    @IsArray()
+    @IsOptional()
+    public privacyInfo: string[];
+
     @ApiProperty()
     @IsArray()
     public members: string[];
+
+    @ApiProperty()
+    @IsString()
+    public owner: string;
 }
--- src/rooms/schema/room.schema.ts
+++ src/rooms/schema/room.schema.ts
@@ -1,12 +1,76 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 import { User } from '../../users/schema';
+import { Owner } from '../../owners/schema';
 @Schema()
 export class Room {
     @Prop({ required: true })
     public title: string;
 
-    @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }] })
+    @Prop({ required: false, nullable: true })
+    public description: string;
+
+    @Prop({ required: false, nullable: true })
+    public extraInfo: string;
+
+    @Prop({ required: true })
+    public furnished: boolean;
+
+    @Prop({ required: false, nullable: true })
+    public houseSize: number;
+
+    @Prop({ required: true })
+    public amountOfRooms: number;
+
+    @Prop({ required: true })
+    public whatIsShared: string;
+
+    @Prop({ required: true })
+    public whatIsPersonal: string;
+
+    @Prop({ required: false, nullable: true })
+    public amountOfStudents: number;
+
+    @Prop({ required: false, nullable: true })
+    public PeopleLivingHere: number;
+
+    @Prop({ required: false, nullable: true })
+    public areTherePets: boolean;
+
+    @Prop({ required: false, nullable: true })
+    public kindOfPets: string;
+
+    @Prop({ required: true })
+    public imageLink: string;
+
+    @Prop({ required: true, type: 'date' })
+    public startDateAvailability: string;
+
+    @Prop({ required: true, type: 'date' })
+    public endDateAvailability: string;
+
+    @Prop({ required: true })
+    public rentPrice: number;
+
+    @Prop({ required: true })
+    public additionalCost: number;
+
+    @Prop({ required: true })
+    public serviceCost: number;
+
+    @Prop({ required: true })
+    public deposit: number;
+
+    @Prop({ required: false, nullable: true })
+    public idealAttendantInfo: string[];
+
+    @Prop({ required: false, nullable: true })
+    public privacyInfo: string[];
+
+    @Prop({ required: true, type: Types.ObjectId, ref: 'Owner' + Owner.name })
+    public owner: Owner;
+
+    @Prop({ required: false, nullable: true, type: [{ type: Types.ObjectId, ref: 'User' + User.name }] })
     public members: User[];
 }
 
