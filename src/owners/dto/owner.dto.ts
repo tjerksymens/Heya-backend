0ec0352fd87569ebe86dd1816ee4ed2d6196da4e
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-03-21 14:29:58 +0100

    feat: expanding the schema and dto of owner with al possible info

--- src/owners/dto/owner.dto.ts
+++ src/owners/dto/owner.dto.ts
@@ -1,11 +1,32 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsEnum, IsString } from 'class-validator';
+import { GenderEnum } from '../enums';
 export class OwnerDto {
     @ApiProperty()
     @IsString()
-    public name: string;
+    public firstName: string;
+
+    @ApiProperty()
+    @IsString()
+    public lastName: string;
+
+    @ApiProperty()
+    @IsEnum(GenderEnum)
+    public type: GenderEnum;
 
     @ApiProperty()
     @IsString()
     public email: string;
+
+    @ApiProperty()
+    @IsString()
+    public password: string;
+
+    @ApiProperty()
+    @IsString()
+    public phoneNumber: string;
+
+    @ApiProperty()
+    @IsString()
+    public owner: string;
 }
--- src/owners/schema/owner.schema.ts
+++ src/owners/schema/owner.schema.ts
@@ -1,13 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
+import { GenderEnum } from '../enums';
+import { Room } from '../../rooms/schema';
 
 @Schema()
 export class Owner {
-    @Prop()
-    public name: string;
+    @Prop({ required: true })
+    public firstName: string;
 
-    @Prop()
-    public contact: string;
+    @Prop({ required: true })
+    public lastName: string;
+
+    @Prop({ enum: GenderEnum })
+    public gender: GenderEnum;
+
+    @Prop({ required: false, nullable: true, type: 'date' })
+    public birthday: string;
+
+    @Prop({ required: true })
+    public email: string;
+
+    @Prop({ required: true })
+    public password: string;
+
+    @Prop({ required: false, nullable: true })
+    public phoneNumber: string;
+
+    @Prop({ required: true, type: Types.ObjectId, ref: 'Room' })
+    public owner: Room;
 }
 
 export type OwnerDocument = Owner & Document;
