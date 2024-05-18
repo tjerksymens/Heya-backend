a62627de72ff4f1ce2e58fe04ec42f76db2036c5
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2024-05-18 13:11:12 +0200

    fix: cors post for users

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
+
+    const corsOptions: CorsOptions = {
         origin: ['http://localhost:5173', 'https://www.heya.me'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: 'Content-Type, Accept',
+        allowedHeaders: 'Authorization,Content-Type',
         credentials: true,
-    });
+    };
+
+    app.enableCors(corsOptions);
+
     await app.listen(3000);
 }
 bootstrap();
